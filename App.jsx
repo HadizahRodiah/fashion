8fd6c37d4dee243ec15b49118bdb7590175745e2
HadizahRodiah <123167408+HadizahRodiah@users.noreply.github.com>
2024-09-16 09:08:42 +0100

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './App.css'
+import Noble from './component/Noble'
+import Car from './component/Car'
+import Welcome from './component/Welcome'
+import Slide from './component/Slide'
+import Contact from './component/Contact'
+import Footer from './component/Footer'
+
+
+function App() {
+  return (
+    <>
+    <Welcome/>
+    <Car/>
+    <Slide/>
+    <Noble/>
+    <Contact/>
+    <Footer/>
+    </>
+  )
+}
+
+export default App
